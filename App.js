0665aaa46e0d818cae3456a09f9a912acda64019
Arandelle <topaguintsarandell@gmail.com>
2024-08-01 21:23:05 +0800

    Fixing the dotenv for firebase config, adding the fetch data if admin from firebase, installing native stack

--- App.js
+++ App.js
@@ -1,11 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState, useEffect } from 'react';
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
 import { Text, View } from 'react-native';
+import { auth } from './src/services/firebaseConfig';
+import { onAuthStateChanged } from 'firebase/auth';
+import { get, getDatabase, ref } from 'firebase/database';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './src/screens/Home';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App = () =>  {
+  const [user, setUser] = useState(null);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        setUser(user);
+        const db = getDatabase();
+        const adminRef = ref(db, `admins/${user.uid}`);
+        const adminSnapshot = await get(adminRef);
+        setIsAdmin(adminSnapshot.exists());
+      } else {
+        setUser(null);
+        setIsAdmin(false);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    )
+  }
+
   return (
-    <View className="w-full h-full flex items-center justify-center">
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View className="w-full h-full flex items-center justify-center">
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,36 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { View,Button,
-} from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { signOut } from "firebase/auth";
-import { auth } from "../services/firebaseConfig";
-import FetchingData from "../services/FetchingData";
-
-const Home = ({ setAuth, badgeSize, setBadgeSize, setIsProfileComplete }) => {
-  const navigation = useNavigation();
-  const handleLogout = async () => {
-    try {
-      await signOut(auth);
-      setAuth(false);
-      navigation.navigate("Login");
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  const handleBadgeSize = () => {
-    setBadgeSize(badgeSize + 1);
-  };
-
+import { View } from "react-native";
+const Home = () => {
 
   return (
     <View>
-      <FetchingData setIsProfileComplete={setIsProfileComplete}/>
       <View className="h-full flex items-center justify-center bg-gray-100">
         <View className="flex flex-row w-full justify-between">
-          <Button title="Logout" onPress={handleLogout} />
-          <Button title="Add Notification" onPress={handleBadgeSize} />
-          <Button title="Go to Responder Screen" onPress={()=> navigation.navigate("ResponderMap")} />
+           
         </View>
       </View>
     </View>
