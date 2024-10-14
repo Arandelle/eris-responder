23762a17ca76abeabd54a7cf95b539af081dc91e
Arandelle <topaguintsarandell@gmail.com>
2024-10-14 12:37:57 +0800

    Redesigned the emergency records list

--- src/screens/Records.js
+++ src/screens/Records.js
@@ -1,62 +1,97 @@
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, Image } from 'react-native';
 import useFetchRecord from '../hooks/useFetchRecord';
 import useFetchUser from '../hooks/useFetchUser';
+import { formatDate } from '../helper/FormatDate';
 
 const Records = ({ status }) => {
   const {emergencyRecords} = useFetchRecord(status);
-  // Sorting history by date
+  // Sorting records by date
   emergencyRecords.sort((a, b) => new Date(b.date) - new Date(a.date));
 
   return (
-    <View className="bg-gray-100 h-full p-2 w-full rounded-lg shadow-lg">
-      <ScrollView>
-        {emergencyRecords.length > 0 ? (
-          emergencyRecords.map((history) => (
-            <RecordItem key={history.id} history={history}/>
-          ))
-        ) : (
-          <Text className="text-center text-gray-500">{`No records found for ${status}`}</Text>
-        )}
-      </ScrollView>
-    </View>
+      <View className="p-2 bg-white">
+        <ScrollView>
+        <View className="space-y-2">
+            {emergencyRecords.length > 0 ? (
+              emergencyRecords.map((records) => (
+                <View className="space-y-2">
+                <RecordItem key={records.id} records={records}/>
+                </View>
+              ))
+            ) : (
+              <Text className="text-center text-gray-500">{`No records found for ${status}`}</Text>
+            )}
+        </View>
+        </ScrollView>
+      </View>
   );
 };
 
-const RecordItem = ({history}) => {
-  const {userDetails} = useFetchUser(history.userId);
+const RecordItem = ({records}) => {
+  const {userDetails} = useFetchUser(records.userId);
 
   
   const emergencyStatus = {
-    "awaiting response": 'bg-yellow-300',
-    "on-ging": 'bg-orange-300',
-    resolved: 'bg-green-300',
+    "awaiting response": 'bg-orange-100 text-orange-600',
+    "on-going": 'bg-blue-100 text-blue-600',
+    resolved: 'bg-green-100 text-green-600',
     expired: 'bg-red-300',
   };
 
   return (
-    <View>
-    <View
-      className={`flex flex-row justify-between p-2 border border-gray-500 ${emergencyStatus[history.status]}`}
-    >
-      <Text className="text-lg font-bold">History ID:</Text>
-      <Text className="text-lg">{history.id}</Text>
+    <View className="border border-gray-300 rounded-lg">
+    <View className="flex flex-row space-x-2 p-4">
+       <Image 
+         source={{ uri: userDetails?.img }} 
+         className="h-12 w-12 rounded-full" 
+       />
+       <View>
+         <Text className="text-lg font-bold">
+           {`${userDetails?.firstname} ${userDetails?.lastname}` || "Loading..."}
+         </Text>
+         <Text className="text-sm text-gray-400">{userDetails?.customId}</Text>
+       </View>
     </View>
-    <View className="space-y-2 p-3">
-      <Text className="text-lg font-bold">
-        Type: {history.type.toUpperCase()}
+ 
+    <View className="mx-2 mb-2 rounded-md p-4 space-y-2 bg-gray-100">
+      <Text className={`font-bold ${emergencyStatus[records.status]} py-1 px-3 rounded-lg self-start`}>
+        {records.status.toUpperCase()}
       </Text>
-      <Text className="text-lg">Name: {`${userDetails?.firstname} ${userDetails?.lastname}` || "Loading..."}</Text>
-      <Text className="text-lg">Description: {history.description}</Text>
-      <Text className="text-lg">Location: {history.location}</Text>
-      <Text className="text-lg">Status: {history.status.toUpperCase()}</Text>
-      <Text className="text-lg">
-        Submitted: {new Date(history.date).toLocaleString()}
-      </Text>
-      <Text className="text-lg">
-        Date Accepted: {new Date(history.dateAccepted).toLocaleString()}
-      </Text>
+ 
+      <View className="space-y-2 p-1">
+        <View className="flex flex-row">
+          <Text className="w-1/3 font-bold text-gray-500">Emergency type:</Text>
+          <Text className="flex-1 font-bold">{records.type.toUpperCase()}</Text>
+        </View>
+ 
+        <View className="flex flex-row">
+          <Text className="w-1/3 font-bold text-gray-500">Description:</Text>
+          <Text className="flex-1 font-bold">{records.description}</Text>
+        </View>
+ 
+        <View className="flex flex-row">
+          <Text className="w-1/3 font-bold text-gray-500">Location:</Text>
+          <Text className="flex-1 font-bold">{records.location}</Text>
+        </View>
+ 
+        <View className="flex flex-row">
+          <Text className="w-1/3 font-bold text-gray-500">Reported At:</Text>
+          <Text className="flex-1 font-bold">{formatDate(records.date)}</Text>
+        </View>
+ 
+        <View className="flex flex-row">
+          <Text className="w-1/3 font-bold text-gray-500">Response Time:</Text>
+          <Text className="flex-1 font-bold">{formatDate(records.dateAccepted)}</Text>
+        </View>
+ 
+        <View className="flex flex-row">
+          <Text className="w-1/3 font-bold text-gray-500">Emergency Id:</Text>
+          <Text className="flex-1 font-bold">{records?.customId}</Text>
+        </View>
+      </View>
     </View>
-  </View>
+ </View>
+ 
   )
 }
 
